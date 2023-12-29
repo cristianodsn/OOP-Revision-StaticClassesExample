9204e9129b4c4690734c7624cabd13ccea8b9e68
Cristiano Castiel <cristianoct03@gmail.com>
2023-12-29 16:51:03 -0300

    Finished example

---
+++ Rascunho/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Rascunho
+{
+    class Calculator
+    {
+        public static double Pi = 3.14;
+
+        public static double Circuference(double r)
+        {
+            return 2.0 * Pi * r;
+        }
+
+        public static double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(r, 3.0);
+        }
+    }
+}
---
+++ Rascunho/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Rascunho
+{
+    class Program
+    {      
+        static void Main(string[] args)
+        {        
+
+            Console.Write("Input the radius value: ");
+            double radius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circuference = Calculator.Circuference(radius);
+            double volume = Calculator.Volume(radius);
+
+            Console.WriteLine("Circuference: " + circuference.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PI value: " + Calculator.Pi.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.ReadKey();
+        }      
+
+    }
+}
